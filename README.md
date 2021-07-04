83b09533c4b44fd2332c7c6e0c2317b1ca753b58
shalldie <hi.xieshuang@gmail.com>
2021-07-04 20:29:35 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dsbyts
+
 balabala...
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+console.log('lll');
+
+function main(): void {
+    const name = 'tom';
+    console.log('hello world');
+    const obj: any = {};
+}
+
+main();
